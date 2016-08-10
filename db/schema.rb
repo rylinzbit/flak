a3182530bf79b18a239328bcf7c92e8dd5be4b63
A Libunao <oldnao@gmail.com>
2016-08-09 23:09:16 -0700

    commit schema modification

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 20160711221415) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "email_subscribers", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "lessons", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
